cea9a1e91e284ec33de01d33e019bdb011fac982
Eric Czajkowski <eric.czajkowski@gmail.com>
2016-06-13 21:09:12 -0400

    update with new logo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,27 +10,4 @@
 angular.module('arboretumApp')
   .controller('MainCtrl', function ($scope) {
 
-    // this.options = {
-    //   interval: 5000,
-    //   template: '../views/templates/carousel.tpl.html'
-    //
-    // };
-
-    // this.slides = [{
-    //     image: '../images/ortho_arboretum.jpg',
-    //     text: 'Simple map of the site',
-    //     id: 0,
-    //   },
-    //   {
-    //     image: '../images/birdseye_arboretum.jpg',
-    //     text: 'Bird\'s eye view of the land, taken from Google Maps',
-    //     id: 1
-    //   },
-    //   {
-    //     image: '../images/streetview_arboretum.jpg',
-    //     text: 'From streetview',
-    //     id: 2
-    //   }
-    // ];
-
   });
